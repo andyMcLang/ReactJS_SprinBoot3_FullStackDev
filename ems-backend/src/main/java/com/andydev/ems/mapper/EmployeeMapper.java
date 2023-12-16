41e29913f3a6d9f9d4642c7eddab5beeb01a500d
Andy Lang <djdreviz@gmail.com>
2023-12-16 17:07:49 +0200

    Create EmployeeDto and EmployeeMapper done

---
+++ ems-backend/src/main/java/com/andydev/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.andydev.ems.mapper;
+
+import com.andydev.ems.dto.EmployeeDto;
+import com.andydev.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
