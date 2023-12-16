747978d3a75078cc16636dcf8f3349ce570e932c
Andy Lang <djdreviz@gmail.com>
2023-12-16 16:23:42 +0200

    Create EmployeeRepository done

--- ems-backend/src/main/java/com/andydev/ems/repository/EmployeeRepository.java
+++ ems-backend/src/main/java/com/andydev/ems/repository/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.andydev.ems.repository;
 
-public interface EmployeeRepository {
+import com.andydev.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
