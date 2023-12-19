4028f72d5a5ade76f44c573f4fc41ae56b42c31d
Andy Lang <djdreviz@gmail.com>
2023-12-19 20:31:53 +0200

    Adding Header and Footer to React App done

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import Headercomponent from "./components/Headercomponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
   return (
     <>
+      <Headercomponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   );
 }
---
+++ full-stack/ems-frontend/src/components/Headercomponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Headercomponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="https://digiosaajandy.wordpress.com/">
+            Employee Management System
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Headercomponent;
